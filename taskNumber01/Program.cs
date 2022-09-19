36bb2b28d17d35a8c6e4c072c7922b0f083ee7a7
Ann <annadiacova@gmail.com>
2022-09-19 10:53:22 +0300

    Task01

---
+++ taskNumber01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ввведите число");
+string input1 = Console.ReadLine();
+int number1 = int.Parse(input1);
+
+Console.WriteLine("Ввведите число");
+string input2 = Console.ReadLine();
+int number2 = int.Parse(input2);
+if(number1>number2)
+{
+    Console.WriteLine($"max={number1},min={number2}");
+}
+else 
+{
+    Console.WriteLine($"min={number1},max={number2}");
+}
+
