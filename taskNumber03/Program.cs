73ebbaec14026c2d70040a766a3db40af77f4f64
Ann <annadiacova@gmail.com>
2022-09-19 11:22:43 +0300

    Task03

---
+++ taskNumber03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number % 2==1)
+{
+    Console.Write("нет, нечётное число");
+}
+else if (number % 2==-1)
+{
+    Console.Write("нет, нечётное число");
+}
+else
+{
+    Console.Write("да, чётное число");
+}
+
